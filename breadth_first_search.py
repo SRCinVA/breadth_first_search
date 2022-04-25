00beda45a545fc4389916b68c7e928d7b8b5de39
SRCinVA <scitarella@hotmail.com>
2022-04-24 21:46:20 -0400

    changed color

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -17,5 +17,13 @@ maze = [
 ]
 
 def main(stdscr):  #this stands for 'standard output screen'
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)  # to implement a color.
+    blue_and_black = curses.color_pair(1) # the color pair you created.
+
+
     stdscr.clear()
-    stdscr.addstr()
+    stdscr.addstr(0,0,"hello world!", blue_and_black) # to pass the position and the text to etner 
+    stdscr.refresh() # to see what we've written
+    stdscr.getch() # "get character" from the user input.
+
+wrapper(main) # initializes the curses module and calls main(). This enables us to control the output.
