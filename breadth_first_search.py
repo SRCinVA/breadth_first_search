a05a0a36c2e6a180ca122ec6bb9646ad6e1c1a4b
SRCinVA <scitarella@hotmail.com>
2022-04-26 20:54:50 -0400

    Improvements to the maze

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -21,17 +21,17 @@ def print_maze(maze, stdscr, path=[]):  # "path" is that ideal shortest path
     RED = curses.color_pair(2) # we'll make the path red.
 
     for i, row in enumerate(maze):  # need to iterate through the maze. "Row" for each individual row, with "i" for which row within the 2D array 
-        for j, value in enumerate:
-            pass
+        for j, value in enumerate(row):  #'value' is whatever symbol is in the column.
+            stdscr.addstr(i,j*2,value, BLUE) # you need these three pieces of info to draw on to the screen.
+                                        # the multiplication spreads the entire stucture out.
 
 def main(stdscr):  #this stands for 'standard output screen'
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)  # to implement a color.
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
-
-    # stdscr.clear()
-    # stdscr.addstr(5,5,"hello world!", blue_and_black) # to pass the position and the text to etner 
-    # stdscr.refresh() # to see what we've written
-    # stdscr.getch() # "get character" from the user input.
+    stdscr.clear()
+    print_maze(maze, stdscr)
+    stdscr.refresh() # to see what we've written
+    stdscr.getch() # "get character" from the user input.
 
 wrapper(main) # initializes the curses module and calls main(). This enables us to control the output.
